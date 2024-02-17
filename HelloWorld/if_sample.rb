517cb4788522896785285c9d0fbe245d46b7137c
ando <leo81@outlook.jp>
2024-02-17 17:52:32 +0900

    if文の山王ルコーど

---
+++ HelloWorld/if_sample.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# if文　基本形
+a = 20
+if a >= 10 then
+  p "bigger"
+else
+  p "smaller"
+end
+
+# if文 thenは省略可能
+a = 2
+if a >= 10
+  p "bigger"
+else
+  p "smaller"
+end
