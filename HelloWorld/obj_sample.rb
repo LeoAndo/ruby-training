67036bc12e41db085d8b561a303b1a3edddfccd4
ando <leo81@outlook.jp>
2024-02-18 21:27:14 +0900

    objectの比較サンプル

---
+++ HelloWorld/obj_sample.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+ary = ["leo", "leo2", "leo3"]
+ary2 = ["leo", "leo2", "leo3"]
+
+p ary.object_id # オブジェクトに割り当てられる一意なID
+p ary2.object_id
+
+# オブジェクトの同一性の比較
+p ary.equal?(ary2) # false
+
+# オブジェクトの同値性の比較
+p ary.eql?(ary2) # true
