6769e8d5ab7fee916501b7ac83d0e3905672729a
ando <leo81@outlook.jp>
2024-02-22 17:39:17 +0900

    Classを作る

--- HelloWorld/class_sample.rb
+++ HelloWorld/class_sample.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class HelloWorld
+  attr_accessor :name # -> @nameのgetter / setterメソッドを自動的に作成
+  # attr_writer :name -> @nameのsetterメソッドを自動的に作成
+  # attr_reader :name -> @nameのgetterメソッドを自動的に作成
   def initialize(name = "Ruby")
     @name = name
   end
@@ -16,5 +19,9 @@ ruby = HelloWorld.new
 
 [bob, alice, ruby].each do |i|
   i.hello
+end
+
+[bob, alice, ruby].each do |i|
+  p i.name
 end
 
