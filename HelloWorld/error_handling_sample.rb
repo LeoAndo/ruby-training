9902b8a97e2a91c71a05d53567ee2b49db26f7b4
ando <leo81@outlook.jp>
2024-03-08 23:34:10 +0900

    update

--- HelloWorld/error_handling_sample.rb
+++ HelloWorld/error_handling_sample.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# カスタムException
+class MyError < StandardError; end
+
+class MySubError1 < MyError; end
+
+class MySubError2 < MyError; end
+
+# 例外をわざと発生させるサンプル
+begin
+  raise MySubError2, "aaasasasメッセージ"
+rescue MyError
+  p $!.message
+ensure
+end
+
 begin
   # 例外を発生させる可能性のある処理
   open "存在しないファイルパス"
@@ -14,9 +29,9 @@ end
 begin
   open "例外を指定した捕捉"
 rescue Errno::ENOENT
-  p "aaaa"
+  p "aaa " + $!.message
 rescue Errno::ECONNRESET, Errno::ConversionError
-  p "bbbb"
+  p "bbb " + $!.message
 ensure
   p "例外の有無に関わらず、必ず呼ばれる処理"
 end
